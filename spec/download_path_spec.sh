f7b01fd83abc086dc95e9f072e00f57bcc0bf5dc
Gerard Hickey <hickey@kinetic-compute.com>
2022-09-20 16:51:54 -0700

    Added testing and pipeline infrastructure

---
+++ spec/download_path_spec.sh
@@ -0,0 +1,57 @@
+
+export ASDF_INSTALL_VERSION=0.21.0
+export ASDF_INSTALL_PATH=/tmp
+
+Describe 'asdf-bat'
+  Include 'bin/install'
+
+  Describe 'get_download_url()'
+    Context "when OS is OSX"
+      Mock 'uname'
+        echo "apple-darwin"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "x86_64"
+      }
+
+      It 'returns correct URL'
+        When call get_download_url "${ASDF_INSTALL_VERSION}" 'bat'
+        The output should equal "https://github.com/noahgorstein/jqp/releases/download/v${ASDF_INSTALL_VERSION}/jqp_${ASDF_INSTALL_VERSION}_Darwin_x86_64.tar.gz"
+      End
+    End
+
+    Context "when OS is Linux on Intel CPU"
+      Mock 'uname'
+        echo "unknown-linux-gnu"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "x86_64"
+      }
+
+      It 'returns correct URL'
+        When call get_download_url "${ASDF_INSTALL_VERSION}" 'bat'
+        The output should equal "https://github.com/noahgorstein/jqp/releases/download/v${ASDF_INSTALL_VERSION}/jqp_${ASDF_INSTALL_VERSION}_Linux_x86_64.tar.gz"
+      End
+    End
+
+    Context "when OS is Linux on ARM CPU"
+      Mock 'uname'
+        echo "unknown-linux-gnueabihf"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "arm"
+      }
+
+      It 'returns correct URL'
+        When call get_download_url "${ASDF_INSTALL_VERSION}" 'bat'
+        The output should equal "https://github.com/noahgorstein/jqp/releases/download/v${ASDF_INSTALL_VERSION}/jqp_${ASDF_INSTALL_VERSION}_Linux_arm64.tar.gz"
+      End
+    End
+  End
+End
---
+++ spec/util_spec.sh
@@ -0,0 +1,101 @@
+
+export ASDF_INSTALL_VERSION=123
+export ASDF_INSTALL_PATH=/tmp
+
+Describe 'asdf-bat'
+  Include 'bin/install'
+
+  Describe 'get_arch()'
+    Context "when CPU is Intel (64 bit)"
+      Mock 'uname'
+        echo "x86_64"
+      End
+
+      It "returns x86_64"
+        When call get_arch
+        The output should equal "x86_64"
+      End
+    End
+
+    Context "when CPU is ARM64"
+      Mock 'uname'
+        echo "arm64"
+      End
+
+      It "returns aarch64"
+        When call get_arch
+        The output should equal "aarch64"
+      End
+    End
+
+
+  End
+
+  Describe 'get_platform()'
+    Context "when OS is OSX"
+      Mock uname
+        echo "Darwin"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "x86_64"
+      }
+
+      It "returns Darwin"
+        When call get_platform
+        The output should equal "Darwin"
+      End
+    End
+
+    Context "when OS is Linux on Intel CPU"
+      Mock uname
+        echo "Linux"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "x86_64"
+      }
+
+      It "returns Linux"
+        When call get_platform
+        The output should equal "Linux"
+      End
+    End
+
+    Context "when OS is Linux on ARM CPU"
+      Mock uname
+        echo "Linux"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "arm64"
+      }
+
+      It "returns Linux"
+        When call get_platform
+        The output should equal "Linux"
+      End
+    End
+
+    Context "when OS is Windows"
+      Mock uname
+        echo "Windows"
+      End
+
+      # Have to cheat here as can not mock uname twice
+      get_arch() {
+        echo "x86_64"
+      }
+
+      It "returns Windows"
+        When call get_platform
+        The output should equal "Windows"
+      End
+    End
+  End
+End
+
+
